752aff4fbdc5abbdc4232c49a7d8766db4f873a1
vaibhav <vaibhavc3040@gmail.com>
2026-08-30 16:46:41 +0530

    fix: Use authenticate named export in dashboardRoutes.js

--- server/src/routes/dashboardRoutes.js
+++ server/src/routes/dashboardRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import dashboardController from '../controllers/dashboardController.js';
-import { authenticateToken } from '../middlewares/authMiddleware.js';
+import { authenticate } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.use(authenticateToken);
+router.use(authenticate);
 router.get('/summary', dashboardController.getSummary);
 
 export default router;
